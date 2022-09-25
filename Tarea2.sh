b44cbfa827b64a76bbf15d678adb0aed618184b2
niedmann89 <ricardo.niedmann.f@mail.pucv.cl>
2022-09-25 17:32:44 -0300

    Acortar codigo de ej 2

--- Tarea2.sh
+++ Tarea2.sh
@@ -97,6 +97,28 @@ echo "------------------------------------------------------"
 echo " El error porcentual despues de 100 iteraciones es 0$error1 %"
 echo " El error porcentual despues de 1000 iteraciones es 0$error2 %"
 echo "------------------------------------------------------"
+echo "Pregunta 3"
+a=$1
+n=$(date +%d)
+m=$(date +%B)
+if [ $a -gt 31 ];then
+				echo "Revisar error en el archivo Error.log"
+				echo "Ingresaste el numero $a" > Error.log
+				echo "Ingresa un numero válido: Entre el 1 y el 31" >> Error.log
+				break
+		elif [ $a = $n ];then
+				echo "los numeros son iguales"
+				echo "Hoy es $a de $m"
+
+		elif [ $a -lt $n ];then
+				echo "el numero $a es menor al numero $n"
+				echo "Han pasado $(($n - $a)) dias desde el $a de $m"
+		elif [ $a -gt $n ];then
+				echo " el numero $a es mayor al numero $n"
+				echo "Faltan $(($a - $n)) dias para el $a de $m"
+				fi
+#Fin pregunta 3
+echo "------------------------------------------------------"
 #Pregunta 4
 
 echo "Pregunta 4"
@@ -129,4 +151,4 @@ for i in $(seq 1 1 60)
 			break
 	fi	
 done
-
+#Fin pregunta 3
